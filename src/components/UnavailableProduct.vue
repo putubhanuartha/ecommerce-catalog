d15a49eafcd42559ce6c56b0c8da7b1d3d1e55d9
putubhanuartha <putuaryabhanu03@gmail.com>
2023-09-01 19:51:22 +0700

    feat: "complete not avail component design"

--- src/components/UnavailableProduct.vue
+++ src/components/UnavailableProduct.vue
@@ -1,12 +1,12 @@
 <template>
 	<div class="container_unavail">
 		<img
-			src=""
+			src="../assets/sad-face.png"
 			alt=""
 		/>
-		<div>
+		<div class="text_container">
 			<p>This product is unavailable to show</p>
-            <button>Next Product</button>
+			<button>Next Product</button>
 		</div>
 	</div>
 </template>
@@ -21,7 +21,38 @@
 	border-radius: 0.625rem;
 	background: #fff;
 	box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-	display: flex;
-	align-items: center;
+	min-height: 25rem;
+}
+.container_unavail img {
+	position: relative;
+	max-width: 43rem;
+	left: 2.5rem;
+	top: 2rem;
+}
+.container_unavail .text_container {
+	min-width: 25rem;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	z-index: 9999;
+	transform: translate(-50%, -50%);
+}
+
+.container_unavail .text_container p {
+	text-align: center;
+}
+.container_unavail .text_container button {
+	margin: 1rem auto;
+	display: block;
+	width: 100%;
+	padding: 0.3rem;
+	border-radius: 0.25rem;
+	border: 3px solid #000;
+	background: #fff;
+	transition: all 0.2s;
+}
+.container_unavail .text_container button:hover{
+	background: #000;
+	color: #fff;
 }
 </style>
